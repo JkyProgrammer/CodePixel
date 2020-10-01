4823d20668c8a2c8e219bf01e09b1adffc714962
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2020-10-01 11:49:13 +0100

    Rewrote environment

--- src/main/CodePixelPanel.java
+++ src/main/CodePixelPanel.java
@@ -3,7 +3,12 @@ package main;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.AbstractQueue;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 
 import javax.swing.JPanel;
 
@@ -11,8 +16,7 @@ public class CodePixelPanel extends JPanel {
 	private static final long serialVersionUID = -2170386884746336930L;
 	
 	boolean isFirstUpdate = true;	// Determines if the whole screen needs a repaint
-	Pixel singleToRefresh;			// The single pixel that needs refreshing
-	
+
 	CodePixelWindow parent;
 	
 	public CodePixelPanel (CodePixelWindow cp) {
@@ -29,25 +33,16 @@ public class CodePixelPanel extends JPanel {
 			g.fillRect (0, 0, parent.frameSize, parent.frameSize);
 			isFirstUpdate = false;
 		}
-		// Paint the single pixel that needs refreshing
-		paintPixel (g, singleToRefresh);
-		// Clear singleToRefresh
-		singleToRefresh = null;
-	}
-
-	// Check if a pixel exists at the defined point
-	public boolean pixelExists (int x, int y) {
-		if (pixels.containsKey(new Point (x, y))) return true;
-		return false;
 	}
 	
 	// Paint a single pixel into the graphics environment
-	public void paintPixel (Graphics g, Pixel p) {
+	public void paintPixel (Point point) {
+		Pixel p = parent.pixels.get(point);
+		Graphics g = getGraphics().create();
 		if (p == null)
-			return;
+			g.setColor(Color.white);
 		g.setColor(p.color);
-		g.fillRect((p.x * parent.pixelSize) + parent.frameSize/2, (p.y * parent.pixelSize) + parent.frameSize/2, parent.pixelSize, parent.pixelSize);
+		g.fillRect((point.x * parent.pixelSize) + parent.frameSize/2, (point.y * parent.pixelSize) + parent.frameSize/2, parent.pixelSize, parent.pixelSize);
 	}
-	
-	public HashMap<Point, Pixel> pixels = new HashMap<Point, Pixel> ();
+
 }
--- src/main/CodePixelWindow.java
+++ src/main/CodePixelWindow.java
@@ -1,17 +1,11 @@
 package main;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import javax.imageio.ImageIO;
 import javax.swing.JCheckBox;
@@ -20,36 +14,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
 
 
 public class CodePixelWindow extends JFrame {
 
 	private static final long serialVersionUID = -8407839062091833852L;
-	
-	// Handle adding a new pixel at a point, with characteristics as defined in the options pane
-	public void addPixel (Point at) {
-		// Work out the internal position of the new pixel
-		int x = (at.x-this.frameSize/2)/this.pixelSize;
-		int y = (at.y-this.frameSize/2)/this.pixelSize;
-		y -= 10;
-
-
-		// Initialise the pixel based on the characteristics defined in the options pane
-		Pixel newPixel = new Pixel (Integer.parseInt(optionsPane.lifeLengthField.getText()), optionsPane.codeField.getText(), x, y, Integer.parseInt(optionsPane.ageLimitField.getText()));
-		newPixel.tint = (float)((float)(optionsPane.colourSlider.getValue())/(float)255);
-		if (!optionsPane.enableLeapBrdBox.isSelected()) newPixel.allowsLeapBreedGene = false;
-		
-		// Queue the pixel to be added
-		pixelsToAdd.add(newPixel);
-		
-		// Paint the pixel
-		this.cpp.singleToRefresh = newPixel;
-		this.cpp.paintComponent(this.cpp.getGraphics().create());
-	}
-
-	ArrayList<Pixel> pixelsToAdd = new ArrayList<Pixel> ();
-	ArrayList<Point> pixelsToRemove = new ArrayList<Point> ();
 
 	public CodePixelWindow(String string) {
 		super (string);
@@ -72,6 +41,36 @@ public class CodePixelWindow extends JFrame {
 	int frameNumber = 0;
 	BufferedImage currentlyWritingImage = null;
 	
+	public HashMap<Point, Pixel> pixels = new HashMap<Point, Pixel> ();
+	public Queue<Point> pixelQueue = new LinkedList<Point> ();
+	
+	// Add a new pixel at a point
+	public void addDefaultPixel (Point p) {
+		if (pixelExists (p.x, p.y)) return;
+		synchronized (pixels) {
+			pixels.put (p, new Pixel (100, "smrtbrd", p.x, p.y, 80));
+		}
+		pixelQueue.add(p);
+		cpp.paintPixel (p);
+	}
+	
+	// Add a new pixel at a point
+	public void addPixel (Pixel pix) {
+		if (pixelExists (pix.x, pix.y)) return;
+		Point p = new Point (pix.x, pix.y);
+		synchronized (pixels) {
+			pixels.put (p, pix);
+		}
+		pixelQueue.add(p);
+		cpp.paintPixel (p);
+	}
+	
+	// Check if a pixel exists at the defined point
+	public boolean pixelExists (int x, int y) {
+		if (pixels.containsKey(new Point (x, y))) return true;
+		return false;
+	}
+	
 	// Called to set up the GUI
 	public void prepareGUI () {
 		// Create a new CodePixelPanel
@@ -80,191 +79,19 @@ public class CodePixelWindow extends JFrame {
 		// Prepare the frame
 		setSize (frameSize, frameSize);
 		setResizable (false);
-		
-		// Add a listener to detect clicking events
-		this.addMouseListener(new MouseListener () {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				//allowsPixelEnactment = true;
-				if (SwingUtilities.isLeftMouseButton(e)) {
-					// Add a new pixel under the mouse
-					addPixel (new Point (e.getX(), e.getY()));
-				} else {
-					// Remove the pixels under the mouse
-					int x = (e.getX()-frameSize/2)/pixelSize;
-					int y = (e.getY()-frameSize/2)/pixelSize;
-					y -= 10;
-					
-					int topLeftX = x - 2;
-					int topLeftY = y - 2;
 
-					// Iterate over the pixels and queue them to be removed
-					for (int b = topLeftY; b <= topLeftY + 4; b++) {
-						for (int a = topLeftX; a <= topLeftX + 4; a++) {
-							Point o = new Point (a, b);
-							pixelsToRemove.add (o);
-							pixelsToAdd.remove(o);
-						}
-					}
-					
-					// Draw over the pixels that are being removed
-					Graphics g = cpp.getGraphics().create();
-					g.setColor(Color.WHITE);
-					g.fillRect(((x-2) * pixelSize) + frameSize/2, ((y-2) * pixelSize) + frameSize/2, pixelSize * 5, pixelSize * 5);
-				}
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {}
-			@Override
-			public void mouseReleased(MouseEvent e) {}
-			@Override
-			public void mouseEntered(MouseEvent e) {}
-			@Override
-			public void mouseExited(MouseEvent e) {}
-		});
-		
-		// Add a listener to detect mouse drag events
-		this.addMouseMotionListener(new MouseMotionListener () {
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				//allowsPixelEnactment = true;
-				if (SwingUtilities.isLeftMouseButton(e)) {
-					// Add a new pixel under the mouse
-					addPixel (new Point (e.getX(), e.getY()));
-				} else {
-					// Remove pixels under the mouse
-					int x = (e.getX()-frameSize/2)/pixelSize;
-					int y = (e.getY()-frameSize/2)/pixelSize;
-					y -= 10;
-					int topLeftX = x - killBrushRadius;
-					int topLeftY = y - killBrushRadius;
-
-					// Iterate over the pixels and queue them to be removed
-					for (int b = topLeftY; b <= topLeftY + (2*killBrushRadius); b++) {
-						for (int a = topLeftX; a <= topLeftX + (2*killBrushRadius); a++) {
-							Point o = new Point (a, b);
-							pixelsToRemove.add (o);
-							pixelsToAdd.remove(o);
-						}
-					}
-					
-					// Draw over the pixels being removed
-					Graphics g = cpp.getGraphics().create();
-					g.setColor(Color.WHITE);
-					g.fillRect(((x-killBrushRadius) * pixelSize) + frameSize/2, ((y-killBrushRadius) * pixelSize) + frameSize/2, pixelSize * ((2*killBrushRadius) + 1), pixelSize * ((2*killBrushRadius) + 1));
-				}
-			}
-
-			@Override
-			public void mouseMoved(MouseEvent e) {}
-		});
-		
-		// Add a listener to detect mouse scroll events
-		this.addMouseWheelListener(new MouseWheelListener () {
-			@Override
-			public void mouseWheelMoved(MouseWheelEvent e) {
-				killBrushRadius += e.getWheelRotation();
-				if (killBrushRadius < 2) killBrushRadius = 2;
-			}
-		});
-
+		addDefaultPixel (new Point (0,0));
 		// Set up and show the options pane
-		optionsPane = new OptionsPane (this);
-		optionsPane.setVisible (true);
+		//optionsPane = new OptionsPane (this);
+		//optionsPane.setVisible (true);
 		
-		timePane = new TimePane (this);
+		//timePane = new TimePane (this);
 		
-		currentlyWritingImage = new BufferedImage (this.frameSize, this.frameSize, BufferedImage.TYPE_INT_ARGB);
+		//currentlyWritingImage = new BufferedImage (this.frameSize, this.frameSize, BufferedImage.TYPE_INT_ARGB);
 	}
 
 	OptionsPane optionsPane;
 	TimePane timePane;
-	
-	// Runs a complete update of all listed pixels
-	public void updatePixels () {
-		long start = System.nanoTime(); // Start the timer
-		
-		// Cache the pixelsToRemove array, clear out the pixelsToRemove array, then remove the pixels that need to be removed
-		Point[] indices = pixelsToRemove.toArray (new Point[] {});
-		pixelsToRemove.clear();
-		for (Point p : indices) {
-			cpp.pixels.remove (p);
-		}
-		
-		if (verboseTimerLogging) System.out.println((System.nanoTime() - start) + " millis for removal.");
-		start = System.nanoTime();
-		
-		// Cache the pixelsToAdd array, clear out the pixelsToAdd array, then add the pixels that need to be added
-		Pixel[] pixes = pixelsToAdd.toArray(new Pixel[] {});
-		pixelsToAdd.clear();
-		for (Pixel p : pixes) {
-			if (p != null) {
-				cpp.pixels.put(new Point (p.x, p.y), p);
-			}
-		}
-		
-		if (verboseTimerLogging) System.out.println((System.nanoTime() - start) + " millis for adding.");
-		start = System.nanoTime();
-
-		// Tell other threads that the pixels array is now in use
-		isMidUpdate = true;
-		// Cache the current list of pixels
-		Graphics g = cpp.getGraphics().create();
-		Graphics2D imgG = currentlyWritingImage.createGraphics();
-		imgG.setColor(Color.white);
-		imgG.fillRect (0, 0, this.frameSize, this.frameSize);
-		
-		Pixel[] iteratingPixels = cpp.pixels.values().toArray(new Pixel[0]);
-		// Iterate over the cached list of pixels
-		for (Pixel p : iteratingPixels) {
-			if (p.remainingLifetime > 0) { // Check if the pixel has any time left
-				if (verboseTimerLogging) System.out.println((System.nanoTime() - start) + " millis for setup.");
-				start = System.nanoTime();
-				// Enact the pixel
-				enact(p);
-				if (verboseTimerLogging) System.out.println((System.nanoTime() - start) + " millis for enactment.");
-				start = System.nanoTime();
-				// Paint the pixel
-				cpp.singleToRefresh  = p;
-				cpp.paintComponent(g);
-				imgG.setColor(p.color);
-				imgG.fillRect((p.x * this.pixelSize) + this.frameSize/2, (p.y * this.pixelSize) + this.frameSize/2, this.pixelSize, this.pixelSize);
-				if (verboseTimerLogging) System.out.println((System.nanoTime() - start) + " millis for painting.");
-				start = System.nanoTime();
-			} else {
-				if (verboseTimerLogging) System.out.println((System.nanoTime() - start) + " millis for setup.");
-				start = System.nanoTime();
-				// Remove the dead pixel from the pixels array
-				cpp.pixels.remove(new Point (p.x, p.y));
-				// Paint over it
-				
-				g.setColor(Color.WHITE);
-				g.fillRect((p.x * this.pixelSize) + this.frameSize/2, (p.y * this.pixelSize) + this.frameSize/2, this.pixelSize, this.pixelSize);
-				
-				imgG.setColor(Color.WHITE);
-				imgG.fillRect((p.x * this.pixelSize) + this.frameSize/2, (p.y * this.pixelSize) + this.frameSize/2, this.pixelSize, this.pixelSize);
-				if (verboseTimerLogging) System.out.println((System.nanoTime() - start) + " millis for killing.");
-				start = System.nanoTime();
-			}
-		}
-		// Tell other threads that the pixels array is now free again
-		if (!cpp.pixels.isEmpty()) {
-			if (isWritingToImageFile) {
-				writeFinalImage();
-			}
-			frameNumber++;
-		}
-		timePane.frameCounter.setText("Frame: " + frameNumber);
-		timePane.pixelCounter.setText("Pixels: " + cpp.pixels.size());
-		
-		isMidUpdate = false;
-	}
-
-	// Convenience for calling from inside contexts where 'this' doesn't point to the right object
-	public void enact (Pixel p) {
-		p.enact (this);
-	}
 	
 	public void writeFinalImage () {
 		try {
@@ -288,10 +115,16 @@ public class CodePixelWindow extends JFrame {
 	public void updateMain () {
 		while (true) {
 			if (allowsPixelEnactment) { // Run a pixel update only if we're allowed to here
-				updatePixels ();
+				Pixel p;
+				synchronized (pixelQueue) {
+					p = pixels.get(pixelQueue.poll());
+				}
+				p.enact(this);
+				synchronized (pixelQueue) {
+					pixelQueue.add(new Point (p.x, p.y));
+				}
 			} else {
 				System.out.println("Waiting...");
-				
 			}
 		}
 	}
